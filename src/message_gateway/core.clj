19a56508d78a3624d2d4c3bf64dcbdef0d8bd480
Thomas Eastman <tjeastman@gmail.com>
2020-09-01 22:39:33 -0700

    add HTTP endpoint with Ring

--- src/message_gateway/core.clj
+++ src/message_gateway/core.clj
@@ -1,6 +1,11 @@
 (ns message-gateway.core
   (:gen-class))
 
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello World"})
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
