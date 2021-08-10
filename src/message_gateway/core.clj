91d8d60127ae96b7b656871ab195ed18245f2ed7
Thomas Eastman <tjeastman@gmail.com>
2021-08-09 19:02:53 -0700

    add validation of configured collector list

--- src/message_gateway/core.clj
+++ src/message_gateway/core.clj
@@ -4,15 +4,20 @@
             [outpace.config :refer [defconfig]])
   (:gen-class))
 
-(defconfig ^:required collectors)
-
 (def collector-fn
   {:counter prometheus/counter
    :histogram prometheus/histogram
    :gauge prometheus/gauge})
 
+(defn collectors-valid? [collectors]
+  (every? #(contains? collector-fn (:type %)) collectors))
+
 (defn collector-init [collector-type collector-key]
   ((collector-type collector-fn) collector-key))
+
+(defconfig
+  ^{:validate [collectors-valid? "invalid collectors"]}
+  ^:required collectors)
 
 (defonce registry
   (reduce
