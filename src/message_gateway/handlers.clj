58a62277ef1edad0d1d8614da1f065d9cc76bf83
Thomas Eastman <tjeastman@gmail.com>
2021-08-15 10:06:26 -0700

    rename handler variables for consistency

--- src/message_gateway/handlers.clj
+++ src/message_gateway/handlers.clj
@@ -2,12 +2,12 @@
   (:require [iapetos.core :as prometheus]
             [taoensso.timbre :refer [debug]]))
 
-(defn handle-double [registry key topic _ payload]
-  (let [val (Double/parseDouble (String. payload "UTF-8"))]
-    (debug (format "obtained value %.10f for topic %s from server" val topic))
-    (prometheus/set registry key val)))
+(defn handle-double [registry collector-key topic _ payload]
+  (let [value (Double/parseDouble (String. payload "UTF-8"))]
+    (debug (format "obtained value %.10f for topic %s from server" value topic))
+    (prometheus/set registry collector-key value)))
 
-(defn handle-int [registry key topic _ payload]
-  (let [val (Integer/parseInt (String. payload "UTF-8"))]
-    (debug (format "obtained value %d for topic %s from server" val topic))
-    (prometheus/set registry key val)))
+(defn handle-int [registry collector-key topic _ payload]
+  (let [value (Integer/parseInt (String. payload "UTF-8"))]
+    (debug (format "obtained value %d for topic %s from server" value topic))
+    (prometheus/set registry collector-key value)))
