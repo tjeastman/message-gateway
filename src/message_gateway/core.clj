d192b66216a918816fd66750d748cf78e7246ab8
Thomas Eastman <tjeastman@gmail.com>
2021-09-11 16:46:06 -0700

    remove default MQTT server hostname

--- src/message_gateway/core.clj
+++ src/message_gateway/core.clj
@@ -10,7 +10,7 @@
 
 (defconfig ^:required metrics-server-port 8080)
 (defconfig ^:required message-server-port 1883)
-(defconfig ^:required message-server-hostname "localhost")
+(defconfig ^:required message-server-hostname)
 (defconfig message-server-username)
 (defconfig message-server-password)
 
@@ -19,12 +19,21 @@
    prometheus/register
    (prometheus/collector-registry) (map collector-init collectors)))
 
-(defonce httpd
-  (prometheus-standalone/metrics-server registry {:port metrics-server-port}))
+;; (defonce httpd
+;;   (prometheus-standalone/metrics-server registry {:port metrics-server-port}))
 
 (defn -main []
   (info "server running")
   (let [conn-str (format "tcp://%s:%d" message-server-hostname message-server-port)
         conn-opts {:username message-server-username :password message-server-password}
         conn (mh/connect conn-str conn-opts)]
+    (mh/subscribe conn {"/cpu/0/percent" 0} (partial handle-double registry :system/cpu/0/percent))
+    (mh/subscribe conn {"/cpu/1/percent" 0} (partial handle-double registry :system/cpu/1/percent))
+    (mh/subscribe conn {"/cpu/2/percent" 0} (partial handle-double registry :system/cpu/2/percent))
+    (mh/subscribe conn {"/cpu/3/percent" 0} (partial handle-double registry :system/cpu/3/percent))
+
+    (mh/subscribe conn {"/cpu/0/freq" 0} (partial handle-double registry :system/cpu/0/freq))
+    (mh/subscribe conn {"/cpu/1/freq" 0} (partial handle-double registry :system/cpu/1/freq))
+    (mh/subscribe conn {"/cpu/2/freq" 0} (partial handle-double registry :system/cpu/2/freq))
+    (mh/subscribe conn {"/cpu/3/freq" 0} (partial handle-double registry :system/cpu/3/freq))
     (info "connected to MQTT server")))
