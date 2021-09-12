38938cacf91a026e70e3070c5e3711359bcb0a2a
Thomas Eastman <tjeastman@gmail.com>
2021-09-12 12:21:38 -0700

    add tests for all parsable formats

--- test/message_gateway/parser_test.clj
+++ test/message_gateway/parser_test.clj
@@ -2,5 +2,14 @@
   (:require [clojure.test :refer :all]
             [message-gateway.parser :refer :all]))
 
+(deftest parse-double-test
+  (is (= ((:double parse-fn) "333.33" ) 333.33)))
+
+(deftest parse-long-test
+  (is (= ((:long parse-fn) "3333333333" ) 3333333333)))
+
 (deftest parse-int-test
   (is (= ((:int parse-fn) "333" ) 333)))
+
+(deftest parse-bool-test
+  (is ((:bool parse-fn) "true" )))
